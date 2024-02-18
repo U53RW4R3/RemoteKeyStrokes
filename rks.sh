7c1d25493b3d0c45d51f036540bb0df6fcb1786a
userware <userware@hackware-os>
2024-02-17 23:23:47 -0500

    WIP

--- rks.sh
+++ rks.sh
@@ -3,7 +3,7 @@
 function check_dependencies() {
     if ! which xdotool &>/dev/null
     then
-        echo "[!] xdotool is missing! Installing dependency."
+        echo "[!] Installing missing dependency..."
         sudo apt install -y xdotool
         exit 1
     fi
@@ -19,11 +19,18 @@ function InputFile {
     done < $local_file
 }
 
-function OutputRemoteFile {
+function Base64 {
     local file_content=$1
-    local remote_file=$2
+    local output_file=$2
+    
+    # TODO: Finish the implementation
+}
 
-    xdotool search --name "$WINDOWNAME" windowfocus windowactivate type "copy con $remote_file"
+function CopyCon {
+    local file_content=$1
+    local output_file=$2
+
+    xdotool search --name "$WINDOWNAME" windowfocus windowactivate type "copy con $output_file"
     xdotool search --name "$WINDOWNAME" windowfocus windowactivate key Return
 
     while read -r line
@@ -35,6 +42,25 @@ function OutputRemoteFile {
     xdotool search --name "$WINDOWNAME" windowfocus windowactivate key Ctrl+Z Return
 }
 
+function OutputRemoteFile {
+    local local_file=$1
+    local remote_file=$2
+    local transfer_type=$3
+
+    # TODO: Fill in the rest of the transfer methods
+    case $transfer_type in
+    	"" | base64)
+    	   Base64 $local_file $remote_file
+    	   ;;
+    	copycon)
+    	   CopyCon $local_file $remote_file
+    	   ;;
+    	*)
+    	   echo -n "Invalid Type Transfer!"
+    	   ;;
+    esac
+}
+
 function usage() {
     cat << EOF
 Usage: $0 [-c <cmdfile> | -i <input> -o <tofile>] [-w <windowname>] [-h]
@@ -42,6 +68,9 @@ Options:
     -c, --cmdfile <cmdfile>     Specify the file containing commands to execute
     -i, --input <input>         Specify the input file to transfer
     -o, --tofile <tofile>       Specify the output file to transfer
+    -t, --type <transfer_type>  Specify the transfer type (base64 is set by
+    				default if not specified)
+
     -w, --windowname <name>     Specify the window name for RDP (FreeRDP is set
                                 by default if not specified)
 
@@ -50,9 +79,9 @@ EOF
     exit 1
 }
 
-long_opts="cmdfile:,input:,tofile:,windowname:,help"
+long_opts="cmdfile:,input:,tofile:,type:,windowname:,help"
 
-OPTS=$(getopt -o "c:i:o:w:h" --long "$long_opts" -n "$(basename "$0")" -- "$@")
+OPTS=$(getopt -o "c:i:o:t:w:h" --long "$long_opts" -n "$(basename "$0")" -- "$@")
 if [ $? != 0 ]
 then
     echo "Failed to parse options... Exiting." >&2
@@ -74,6 +103,10 @@ while true; do
         -o | --tofile)
             OUTPUT=$2
             shift 2
+            ;;
+	-t | --type)
+            TYPE=$2
+	    shift 2
             ;;
         -w | --windowname)
             WINDOWNAME=$2
@@ -99,18 +132,18 @@ function main() {
     if [ -z "$WINDOWNAME" ]
     then
         WINDOWNAME="FreeRDP"
-    elif [[ "$WINDOWNAME" != "FreeRDP" && "$WINDOWNAME" != "rdesktop" ]]
+    elif [[ "$WINDOWNAME" != "FreeRDP" && "$WINDOWNAME" != "rdesktop"  != "TightVNC" ]]
     then
-        echo "Invalid window name specified. Allowed values: 'FreeRDP' or 'rdesktop'."
+        echo "Invalid window name specified. Allowed values: 'FreeRDP', 'rdesktop', or 'TightVNC'."
         exit 1
     fi
 
     if [ -n "$CMDFILE" ]
     then
         InputFile "$CMDFILE"
-    elif [ -n "$INPUT" ] && [ -n "$OUTPUT" ]
+    elif [ -n "$INPUT" ] && [ -n "$OUTPUT" ] && [ -n "$TYPE" ]
     then
-        OutputRemoteFile "$INPUT" "$OUTPUT"
+        OutputRemoteFile "$INPUT" "$OUTPUT" "$TYPE"
     fi
 }
 
